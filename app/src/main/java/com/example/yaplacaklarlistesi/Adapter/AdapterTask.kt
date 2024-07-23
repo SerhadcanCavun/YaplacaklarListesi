62d66e25b0dc9a19a69af6b5b00063f3fc298d58
Emirhan Kavgacı <93542883+emirhankavgaci@users.noreply.github.com>
2024-07-23 16:55:06 +0300

    layouts improved and adapter used

--- app/src/main/java/com/example/yaplacaklarlistesi/Adapter/AdapterTask.kt
+++ app/src/main/java/com/example/yaplacaklarlistesi/Adapter/AdapterTask.kt
@@ -1,6 +1,7 @@
 package com.example.yaplacaklarlistesi.Adapter
 
 import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
@@ -8,30 +9,34 @@ import android.widget.ImageView
 import android.widget.RadioButton
 import android.widget.TextView
 import androidx.cardview.widget.CardView
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.example.yaplacaklarlistesi.R
 import com.example.yaplacaklarlistesi.TaskItem
 
 class AdapterTask(
-    private var taskItems: List<TaskItem>,
+    private var taskItems: List<TaskItem>
 ) : RecyclerView.Adapter<AdapterTask.TaskViewHolder>() {
 
     class TaskViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        private val radioButton : RadioButton = view.findViewById(R.id.radioButton)
-        private val textView : TextView = view.findViewById(R.id.textView)
+         val radioButton : RadioButton = view.findViewById(R.id.radioButton)
+      val textView : TextView = view.findViewById(R.id.textView)
 
     }
 
      override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskViewHolder {
-         TODO("Not yet implemented")
+         val view = LayoutInflater.from(parent.context).inflate(R.layout.task_item, parent, false)
+         return TaskViewHolder(view)
      }
 
      override fun getItemCount(): Int {
-         TODO("Not yet implemented")
+         return taskItems.count()
      }
 
      override fun onBindViewHolder(holder: TaskViewHolder, position: Int) {
-         TODO("Not yet implemented")
+         val taskItem = taskItems[position]
+         holder.textView.text =taskItem.task_text
+         holder.radioButton.isChecked = taskItem.isDone
      }
 
 
--- app/src/main/java/com/example/yaplacaklarlistesi/MainActivity.kt
+++ app/src/main/java/com/example/yaplacaklarlistesi/MainActivity.kt
@@ -5,8 +5,15 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.yaplacaklarlistesi.Adapter.AdapterTask
 
 class MainActivity : AppCompatActivity() {
+    lateinit var taskItemTest : List<TaskItem>
+    lateinit var taskAdapter : AdapterTask
+    lateinit var recyclerView : RecyclerView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +23,17 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+            taskItemTest = listOf(
+                TaskItem("a",true),
+                TaskItem("b",true),
+                TaskItem("c",true),
+                TaskItem("d",true),
+            )
+
+
+        recyclerView = findViewById(R.id.recyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        taskAdapter = AdapterTask(taskItemTest)
+        recyclerView.adapter = taskAdapter
     }
 }
