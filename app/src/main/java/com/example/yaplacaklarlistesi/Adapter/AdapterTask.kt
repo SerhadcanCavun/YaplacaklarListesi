73b749a645902eb44c8f033022447eb9b0bb66fe
Emirhan Kavgacı <93542883+emirhankavgaci@users.noreply.github.com>
2024-07-23 12:01:03 +0300

    updated adapter

--- app/src/main/java/com/example/yaplacaklarlistesi/Adapter/AdapterTask.kt
+++ app/src/main/java/com/example/yaplacaklarlistesi/Adapter/AdapterTask.kt
@@ -5,19 +5,21 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.RadioButton
 import android.widget.TextView
 import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.yaplacaklarlistesi.R
+import com.example.yaplacaklarlistesi.TaskItem
 
- class AdapterTask(
-    private val context: Context,
-    private val isDone : Boolean,
-    private var taskItems: List<CardView>,
+class AdapterTask(
+    private var taskItems: List<TaskItem>,
 ) : RecyclerView.Adapter<AdapterTask.TaskViewHolder>() {
 
     class TaskViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-      private val taskItem : CardView = view.findViewById(R.id.taskItem)
+        private val radioButton : RadioButton = view.findViewById(R.id.radioButton)
+        private val textView : TextView = view.findViewById(R.id.textView)
+
     }
 
      override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskViewHolder {
